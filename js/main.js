aad7e8c5e7e8c90a2b5a1ec9aa42e586539a079b
nashmaniac <Nash@Singularity.local>
2015-03-19 00:07:16 +0530

    Create object for the note, thus improving code,

--- js/main.js
+++ js/main.js
@@ -1,33 +1,80 @@
 $(document).ready(function(){
 	'use strict';
 
-	var sampleNote = document.querySelector(".itemOne h1");
-	var words = sampleNote.innerHTML.trim().split(" ");
+	// ####################################### START NOTE OBJECT DEFINITON #####################################
 
-	var result = [];
-	words.forEach(function(word, index) {
-		result.push(addSpan(word, "word_" + (index + 1)));
-	});
+	/*
+	* Object : Note
+	* -----------------------------------------------------
+	* Object that defines a single note.
+	*/
+	function Note(noteText) {
+		this.noteText = noteText;
+		this.wordsArray = noteText.trim().split(" ");
+		this.hiddenWordIndices = [1, 4, 9, , 14];
 
-	var finalHTML = result.join(" ");
-	sampleNote.innerHTML = finalHTML;
-
-	function addSpan(string, idName) {
-		return '<span id = "' + idName + '">' + string + "</span>";
+		// Note with the same text stored in HTML form, with span tags around each word, 
+		// tags differ for the words that are supposed to be hidden and those that are not
+		var that = this;
+		this.noteHTML = this.wordsArray.map(function(word, index) {
+			if(that.hiddenWordIndices.indexOf(index) > -1) // the word is supposed to be hidden
+				return '<span id = "word_hidden_' + index + '"  class = "hidden_word">' + word + '</span>';
+			else
+				return '<span id = "word_' + index + '">' + word + '</span>';
+		}).join(" ");
 	}
 
-	var children_words = sampleNote.childNodes;
+	/*
+	* Function : getNoteHTML
+	* -----------------------------------------------------
+	* Returns the HTML version of the note, that is with each
+	* word surrounded by appropriate span tag
+	*/
+	Object.defineProperty(Note.prototype, "getNoteHTML", {get : function() {
+		return this.noteHTML;
+	}});
 
+	/*
+	* Function : isHiddenWord
+	* -----------------------------------------------------
+	* Returns true if the supplied word is supposed to be 
+	* hidden, false otherwise
+	*/
+	Note.prototype.isHiddenWord = function(string) {
+		return (this.hiddenWordIndices.indexOf(this.wordsArray.indexOf(string)) > -1);
+	};
+
+	// ####################################### END NOTE OBJECT DEFINITON #####################################
+
+	// ####################################### MISC. FUNCTION DEFINITIONS ####################################
+
+	/*
+	* Function : randomDelay
+	* Usage    : div.delay(randomDelay()).animate(...)
+	* -------------------------------------------------------
+	* Returns a random number between min_delay and max_delay
+	*/
 	var min_delay = 200;
 	var max_delay = 700;
-	// Now we will add the bokeh effect to this shit right over here
 	function randomDelay() {
 		return Math.floor(Math.random() * (max_delay - min_delay + 1) + min_delay);
 	}
 
-	for(var i = 0 ; i < children_words.length ; i++)
-		$(children_words[i]).delay(randomDelay()).animate({"color" : "black"}, 2000,'easeOutQuint', function(){});
+	// ####################################### END MISC. FUNCTION DEFINITIONS ################################
 
 
+	var testerNote = new Note("Some words that are here they need to be omitted just for fun and giggles.");
+	console.log(testerNote.isHiddenWord("that"));
 
-}());
+	var note = document.querySelector(".note h1");
+	note.innerHTML = testerNote.getNoteHTML;
+	console.log(note);
+
+	var words = note.childNodes;
+
+	for(var i = 0 ; i < words.length ; i++){
+		if(!testerNote.isHiddenWord(words[i].textContent)){
+			$(words[i]).delay(randomDelay()).animate({"color" : "black"}, 2000,'easeOutQuint', function(){});
+		}
+	}
+});
