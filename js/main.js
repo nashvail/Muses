9af8f747104a3bc5f473b53794665eccd6d4493d
nashmaniac <Nash@Singularity.local>
2015-03-18 14:06:53 +0530

    Added jquery ui library

--- js/main.js
+++ js/main.js
@@ -1,42 +1,33 @@
-'use strict';
-var sample_note = $('.itemOne h1');
-
-/*
-* The note object that is to be retreived from the data base
-*/
-function Note(text) {
-	this.text = text;
-	this.text_array = text.split(" ");
-	this.hiddenIndices = [1, 4, 9, 14];
-}
-
-var sampleNote = new Note("Some words that are here they need to be omitted just for fun and giggles.");
-
-
-function processText(note) {
-	var result = [];
-	note.text_array.forEach(function(word, index) {
-		if(note.hiddenIndices.indexOf(index) > -1) // if the current word is to be hidden
-			result.push(replaceWith(word, "_"));
-		else
-			result.push(word);
-	}); 
-	
-	return result.join(" ");
-}
-
-function replaceWith(string, character) {
-	var resultingString = "";
-	for(var i = 0 ; i < string.length ; i++)
-		resultingString += character;
-	return resultingString;
-}
-
-sample_note.append(processText(sampleNote));
-sample_note.hide();
-
 $(document).ready(function(){
-	sample_note.delay(800).fadeIn(1500,"easeOutQuint", function(){
-		// Animation complete
+	'use strict';
+
+	var sampleNote = document.querySelector(".itemOne h1");
+	var words = sampleNote.innerHTML.trim().split(" ");
+
+	var result = [];
+	words.forEach(function(word, index) {
+		result.push(addSpan(word, "word_" + (index + 1)));
 	});
-});
+
+	var finalHTML = result.join(" ");
+	sampleNote.innerHTML = finalHTML;
+
+	function addSpan(string, idName) {
+		return '<span id = "' + idName + '">' + string + "</span>";
+	}
+
+	var children_words = sampleNote.childNodes;
+
+	var min_delay = 200;
+	var max_delay = 700;
+	// Now we will add the bokeh effect to this shit right over here
+	function randomDelay() {
+		return Math.floor(Math.random() * (max_delay - min_delay + 1) + min_delay);
+	}
+
+	for(var i = 0 ; i < children_words.length ; i++)
+		$(children_words[i]).delay(randomDelay()).animate({"color" : "black"}, 2000,'easeOutQuint', function(){});
+
+
+
+}());
