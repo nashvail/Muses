42df65a1215c86b91211760c02cfb73c9422296b
nashmaniac <Nash@Singularity.local>
2015-03-22 00:48:21 +0530

    Made improvement to the javascript code

--- js/main.js
+++ js/main.js
@@ -11,16 +11,16 @@ $(document).ready(function(){
 	function Note(noteText) {
 		this.noteText = noteText;
 		this.wordsArray = noteText.trim().split(" ");
-		this.hiddenWordIndices = [2, 4, 9, , 14];
+		this.hiddenWordIndices = [2, 4, 14, 9];
 
 		// Note with the same text stored in HTML form, with span tags around each word, 
 		// tags differ for the words that are supposed to be hidden and those that are not
 		var that = this;
-		this.noteHTML = this.wordsArray.map(function(word, index) {
-			if(that.hiddenWordIndices.indexOf(index) > -1) // the word is supposed to be hidden
-				return '<span id = "word_hidden_' + index + '"  class = "hidden_word">' + word + '</span>';
+		this.noteHTML = this.wordsArray.map(function(word, currentWordIndex) {
+			if(that.hiddenWordIndices.indexOf(currentWordIndex) > -1) // the word is supposed to be hidden
+				return '<span class = "hidden_word">' + word + '</span>';
 			else
-				return '<span id = "word_' + index + '">' + word + '</span>';
+				return '<span id = "word_' + currentWordIndex + '">' + word + '</span>';
 		}).join(" ");
 	}
 
@@ -40,13 +40,50 @@ $(document).ready(function(){
 	* Returns true if the supplied word is supposed to be 
 	* hidden, false otherwise
 	*/
-	Note.prototype.isHiddenWord = function(string) {
-		return (this.hiddenWordIndices.indexOf(this.wordsArray.indexOf(string)) > -1);
+	Note.prototype.isHiddenWord = function(wordIndex) {
+		return (this.hiddenWordIndices.indexOf(wordIndex) > -1);
 	};
 
 	// ####################################### END NOTE OBJECT DEFINITON #####################################
 
 	// ####################################### MISC. FUNCTION DEFINITIONS ####################################
+
+	/*
+	* Function : initialize
+	* -------------------------------------------------------------------
+	* Sets up the viewport by placing respective elements in their places 
+	* and adding event Listeners to the appropriate nodes.
+	* 'noteObj' is a note object
+	*/
+	function initialize(noteObj) {
+		// Get the node holding the note form the DOM
+		var note = document.querySelector(".note h1");
+		console.log(note.childNodes);
+		// clear and add new html to the note holder
+		note.innerHTML = "";
+		note.innerHTML = noteObj.getNoteHTML;
+		// Animate in the note
+		animateNote(note, noteObj);
+
+		// Position the highlight behind the Note
+		var noteContainer = note.getBoundingClientRect();
+		var backHilite = document.querySelector(".backhilite");
+		backHilite.style.top = noteContainer.top;
+		backHilite.style.height = noteContainer.height;
+		backHilite.style.width = 0;
+
+
+		// Event listener for the show button(the bulb icon)
+		var show_btn = $('.show_btn');
+		show_btn.click(function(){
+			show_btn.addClass("cbutton--click");
+			$('.hidden_word').css({"border" : "none"});
+			$('.backhilite').animate({"width" : "100%"}, 700, 'easeInOutCirc', function(){});
+			setTimeout(function(){
+				show_btn.removeClass("cbutton--click");
+			}, 500);
+		});
+	}
 
 	/*
 	* Function : randomDelay
@@ -60,40 +97,40 @@ $(document).ready(function(){
 		return Math.floor(Math.random() * (max_delay - min_delay + 1) + min_delay);
 	}
 
+	/*
+	* Function : animateNote()
+	* ----------------------------------------------------------
+	* Animates the current note by fading in each word at a time
+	* Supplied argument noteNode is the node, that holds the note
+	*/
+	function animateNote(noteNode, noteObj) {
+		var note_words = noteNode.childNodes;
+		// added a 300 milliseconds delay for smoother animation perception
+		setTimeout(function(){
+			for(var i = 0 ; i < note_words.length ; i+=2){
+				// As there are text nodes in between span nodes therefore the actual index of word is the
+				// index / 2
+				if(!noteObj.isHiddenWord(i/2)){
+					$(note_words[i]).delay(randomDelay()).animate({"color" : "#34495e"}, 700, function(){});
+				}
+			}
+		}, 300);
+	}
+
 	// ####################################### END MISC. FUNCTION DEFINITIONS ################################
 
+	var notes = [
+		"All of the thriteen colonies began demanding indpendence in 1775 and gainded in 1776",
+		"The more you sleep the better your grades will get as research says for example",
+		"Due to the shooting up of Mitosin hormone you are not able to sleep prpoerly when there is light."
+	];
 
-	var testerNote = new Note("All of thirteen colonies began demanding independence in 1775 and gained in 1776");
-	console.log(testerNote.isHiddenWord("that"));
+	var testerNote = new Note(notes[Math.floor(Math.random() * notes.length)]);
+	initialize(testerNote);
 
-	var note = document.querySelector(".note h1");
-	note.innerHTML = testerNote.getNoteHTML;
-	console.log(note);
-
-	var words = note.childNodes;
-	// set under settimeout function to smooth the animation a little bit
-	setTimeout(function(){
-		for(var i = 0 ; i < words.length ; i++){
-			if(!testerNote.isHiddenWord(words[i].textContent)){
-				$(words[i]).delay(randomDelay()).animate({"color" : "#34495e"}, 700, function(){});
-			}
-		}
-	}, 300);
-
-	var note = document.querySelector(".note");
-	var noteContainer = note.getBoundingClientRect();
-
-	var backHilite = document.querySelector(".backhilite");
-	backHilite.style.top = noteContainer.top;
-	backHilite.style.height = noteContainer.height;
-	backHilite.style.width = 0;
-
-	var show_btn = $('.show_btn');
-	show_btn.click(function(){
-		show_btn.addClass("cbutton--click");
-		$('.backhilite').animate({"width" : "100%"}, 700, 'easeInOutCirc', function(){});
-		setTimeout(function(){
-			show_btn.removeClass("cbutton--click");
-		}, 500);
+	addEventListener("click", function() {
+		testerNote = new Note(notes[Math.floor(Math.random() * notes.length)]);
+		initialize(testerNote);
 	});
+	
 });
